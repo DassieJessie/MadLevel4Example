7aec34524f7285f44753d7411da1cc5137b4a0cc
Jienie12 <jr.egmond@gmail.com>
2020-11-04 16:21:40 +0100

    1.3 Creating the Database

---
+++ ReminderFragment/app/src/main/java/com/example/reminderfragment/ReminderRoomDatabase.kt
@@ -0,0 +1,41 @@
+package com.example.reminderfragment
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Reminder::class], version = 1, exportSchema = false) // tells the class this is a room database
+abstract class ReminderRoomDatabase : RoomDatabase() {
+
+    abstract fun reminderDao(): ReminderDao
+
+    companion object{
+        private const val DATABASE_NAME = "REMINDER_DATABASE"
+
+        @Volatile
+        private var reminderRoomDatabaseInstance: ReminderRoomDatabase? = null
+
+        fun getDatabase(context: Context): ReminderRoomDatabase? { //Because we want the database to be static we encapsulate the getDatabase function within a companion object.
+
+            if (reminderRoomDatabaseInstance == null) {
+
+                //build database
+                synchronized(ReminderRoomDatabase::class.java) {
+                    if (reminderRoomDatabaseInstance == null) {
+                        reminderRoomDatabaseInstance = Room.databaseBuilder(
+                            context.applicationContext,
+                            ReminderRoomDatabase::class.java, DATABASE_NAME
+                        )
+                            .allowMainThreadQueries()
+                            .build()
+                    }
+                }
+                
+            }
+            return reminderRoomDatabaseInstance //get database
+        }
+
+    }
+
+}
