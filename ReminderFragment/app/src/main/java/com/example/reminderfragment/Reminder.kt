d1eb74d30a8baf4a94757d3e1b17725a416c293c
Jienie12 <jr.egmond@gmail.com>
2020-11-04 16:06:11 +0100

    1.1 Implementing database
    - Modify Reminder data class

--- ReminderFragment/app/src/main/java/com/example/reminderfragment/Reminder.kt
+++ ReminderFragment/app/src/main/java/com/example/reminderfragment/Reminder.kt
@@ -1,5 +1,18 @@
 package com.example.reminderfragment
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+//Room creates an SQLite database using all objects annotated with @Entity
+
+@Entity(tableName = "reminderTable") //this is an entity that needs to be stored in a database
 data class Reminder (
-    var reminderText: String
+
+    @ColumnInfo(name = "reminder") //if you only type @ColumnInfo it takes the variables name as default
+    var reminderText: String,
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo
+    var id: Long? = null
 )
